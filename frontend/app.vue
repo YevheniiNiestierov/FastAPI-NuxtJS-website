fa1aaedc4b94849da6b8edfe9c19776f47c3fb60
Yevhenii <yniestierov@gmail.com>
2026-04-20 22:46:04 +0300

    FIX HYDRATION ISSUE

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,20 +1,10 @@
 <template>
-  <nav>
-    <router-view></router-view>
-<!--    <router-link to="/manager">Go Manager</router-link>-->
-  </nav>
-<!--  <CustomerView/>-->
+  <div id="app">
+    <NuxtPage />
+  </div>
 </template>
 
 <script setup>
-// import CustomerView from './components/customer.vue'
-
-// <!--export default {-->
-// <!--  name: 'App',-->
-// <!--  components: {-->
-// <!--    CustomerView,-->
-// <!--  }-->
-// <!--}-->
 </script>
 
 <style>
@@ -22,8 +12,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* Removed text-align center and margin so it doesn't break your grid layouts */
 }
 </style>
