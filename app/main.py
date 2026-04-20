36f0e292c7521ee22d0a8ef36882aabcea32574e
Yevhenii <yniestierov@gmail.com>
2026-04-21 00:01:57 +0300

    Implement rate limiting for login endpoint and update dependencies

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from app.postgress.database import Base, engine
 from app.products import router as product_router
@@ -10,7 +14,10 @@ from app.order import router as order_router
 from app.images import router as image_router
 from app.auth import router as auth_router
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 origins = [
     "http://localhost:3000",
